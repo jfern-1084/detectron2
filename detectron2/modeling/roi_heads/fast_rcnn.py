2ef843a6817989843c3ef8e17b3e7343b2d10dde
Johan <johan.27011993@gmail.com>
2020-06-18 03:58:39 -0400

    Delta values perfect for DIOU

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -345,160 +345,155 @@ class FastRCNNOutputs:
 
         return loss
 
+    #Bounding boxes instead of delta values for loss calculation
+    #Borrowed from mmdet
+    # def compute_giou(self):
+    #     """
+    #     Generalized Intersection over Union: A Metric and A Loss for
+    #     Bounding Box Regression
+    #     https://arxiv.org/abs/1902.09630
+    #     code refer to:
+    #     https://github.com/sfzhang15/ATSS/blob/master/atss_core/modeling/rpn/atss/loss.py#L36
+    #     Args:
+    #         pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
+    #             shape (n, 4).
+    #         target (Tensor): Corresponding gt bboxes, shape (n, 4).
+    #         eps (float): Eps to avoid log(0).
+    #     Return:
+    #         Tensor: Loss tensor.
+    #     """
+    #
+    #     #Using Predictions instead of proposals
+    #     pred = self.proposals.tensor
+    #     # pred = apply_deltas_broadcast(
+    #     #     self.box2box_transform, self.pred_proposal_deltas, self.proposals.tensor
+    #     # )
+    #     target = self.gt_boxes.tensor
+    #     eps = 1e-7
+    #
+    #     bg_class_ind = self.pred_class_logits.shape[1] - 1
+    #     box_dim = target.size(1)  # 4 or 5
+    #
+    #     fg_inds = torch.nonzero(
+    #         (self.gt_classes >= 0) & (self.gt_classes < bg_class_ind), as_tuple=True
+    #     )[0]
+    #
+    #     gt_class_cols = torch.arange(box_dim, device=self.pred_proposal_deltas.device)
+    #
+    #     pred = pred[fg_inds[:, None], gt_class_cols]
+    #     target = target[fg_inds]
+    #
+    #     # set_trace()
+    #
+    #     # overlap
+    #     lt = torch.max(pred[:, :2], target[:, :2])
+    #     rb = torch.min(pred[:, 2:], target[:, 2:])
+    #     wh = (rb - lt).clamp(min=0)
+    #     overlap = wh[:, 0] * wh[:, 1]
+    #
+    #     # union
+    #     ap = (pred[:, 2] - pred[:, 0]) * (pred[:, 3] - pred[:, 1])
+    #     ag = (target[:, 2] - target[:, 0]) * (target[:, 3] - target[:, 1])
+    #     union = ap + ag - overlap + eps
+    #
+    #     # IoU
+    #     ious = overlap / union
+    #
+    #     # enclose area
+    #     enclose_x1y1 = torch.min(pred[:, :2], target[:, :2])
+    #     enclose_x2y2 = torch.max(pred[:, 2:], target[:, 2:])
+    #     enclose_wh = (enclose_x2y2 - enclose_x1y1).clamp(min=0)
+    #     enclose_area = enclose_wh[:, 0] * enclose_wh[:, 1] + eps
+    #
+    #
+    #     #Borrowed from sl1
+    #     # bg_class_ind = self.pred_class_logits.shape[1] - 1
+    #     #
+    #     # fg_inds = torch.nonzero(
+    #     #     (self.gt_classes >= 0) & (self.gt_classes < bg_class_ind), as_tuple=True
+    #     # )[0]
+    #
+    #
+    #     # GIoU
+    #     gious = ious - (enclose_area - union) / enclose_area
+    #     # loss = (1 - gious)  #From original
+    #     loss = ((1 - gious).sum() / self.gt_classes.numel())
+    #     loss = loss * self.cfg.MODEL.ROI_BOX_HEAD.LOSS_BOX_WEIGHT
+    #
+    #     return loss
 
-    def compute_giou(self):
-        """
-        Generalized Intersection over Union: A Metric and A Loss for
-        Bounding Box Regression
-        https://arxiv.org/abs/1902.09630
-        code refer to:
-        https://github.com/sfzhang15/ATSS/blob/master/atss_core/modeling/rpn/atss/loss.py#L36
-        Args:
-            pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
-                shape (n, 4).
-            target (Tensor): Corresponding gt bboxes, shape (n, 4).
-            eps (float): Eps to avoid log(0).
-        Return:
-            Tensor: Loss tensor.
-        """
 
-        #Using Predictions instead of proposals
-        pred = self.proposals.tensor
-        # pred = apply_deltas_broadcast(
-        #     self.box2box_transform, self.pred_proposal_deltas, self.proposals.tensor
-        # )
-        target = self.gt_boxes.tensor
-        eps = 1e-7
-
-        bg_class_ind = self.pred_class_logits.shape[1] - 1
-        box_dim = target.size(1)  # 4 or 5
-
-        fg_inds = torch.nonzero(
-            (self.gt_classes >= 0) & (self.gt_classes < bg_class_ind), as_tuple=True
-        )[0]
-
-        gt_class_cols = torch.arange(box_dim, device=self.pred_proposal_deltas.device)
-
-        pred = pred[fg_inds[:, None], gt_class_cols]
-        target = target[fg_inds]
-
-        # set_trace()
-
-        # overlap
-        lt = torch.max(pred[:, :2], target[:, :2])
-        rb = torch.min(pred[:, 2:], target[:, 2:])
-        wh = (rb - lt).clamp(min=0)
-        overlap = wh[:, 0] * wh[:, 1]
-
-        # union
-        ap = (pred[:, 2] - pred[:, 0]) * (pred[:, 3] - pred[:, 1])
-        ag = (target[:, 2] - target[:, 0]) * (target[:, 3] - target[:, 1])
-        union = ap + ag - overlap + eps
-
-        # IoU
-        ious = overlap / union
-
-        # enclose area
-        enclose_x1y1 = torch.min(pred[:, :2], target[:, :2])
-        enclose_x2y2 = torch.max(pred[:, 2:], target[:, 2:])
-        enclose_wh = (enclose_x2y2 - enclose_x1y1).clamp(min=0)
-        enclose_area = enclose_wh[:, 0] * enclose_wh[:, 1] + eps
-
-
-        #Borrowed from sl1
-        # bg_class_ind = self.pred_class_logits.shape[1] - 1
-        #
-        # fg_inds = torch.nonzero(
-        #     (self.gt_classes >= 0) & (self.gt_classes < bg_class_ind), as_tuple=True
-        # )[0]
-
-
-        # GIoU
-        gious = ious - (enclose_area - union) / enclose_area
-        # loss = (1 - gious)  #From original
-        loss = ((1 - gious).sum() / self.gt_classes.numel())
-        loss = loss * self.cfg.MODEL.ROI_BOX_HEAD.LOSS_BOX_WEIGHT
-
-        return loss
-
-
-
-    def compute_diou_fvcore(self):
-
-        box_dim = self.gt_boxes.tensor.size(1)  # 4 or 5
-        device = self.pred_proposal_deltas.device
-        bg_class_ind = self.pred_class_logits.shape[1] - 1
-        gt_class_cols = torch.arange(box_dim, device=device)
-
-        fg_inds = nonzero_tuple((self.gt_classes >= 0) & (self.gt_classes < bg_class_ind))[0]
-
-        boxes1 = self._predict_boxes()[fg_inds[:, None], gt_class_cols]
-        boxes2 = self.gt_boxes.tensor[fg_inds]
-        eps = 1e-7
-
-        x1, y1, x2, y2 = boxes1.unbind(dim=-1)
-        x1g, y1g, x2g, y2g = boxes2.unbind(dim=-1)
-
-        assert (x2 >= x1).all(), "bad box: x1 larger than x2"
-        assert (y2 >= y1).all(), "bad box: y1 larger than y2"
-
-        # Intersection keypoints
-        xkis1 = torch.max(x1, x1g)
-        ykis1 = torch.max(y1, y1g)
-        xkis2 = torch.min(x2, x2g)
-        ykis2 = torch.min(y2, y2g)
-
-        intsctk = torch.zeros_like(x1)
-        mask = (ykis2 > ykis1) & (xkis2 > xkis1)
-        intsctk[mask] = (xkis2[mask] - xkis1[mask]) * (ykis2[mask] - ykis1[mask])
-        unionk = (x2 - x1) * (y2 - y1) + (x2g - x1g) * (y2g - y1g) - intsctk
-        iouk = intsctk / (unionk + eps)
-
-        # smallest enclosing box
-        xc1 = torch.min(x1, x1g)
-        yc1 = torch.min(y1, y1g)
-        xc2 = torch.max(x2, x2g)
-        yc2 = torch.max(y2, y2g)
-
-        x_p = (x2 + x1) / 2
-        y_p = (y2 + y1) / 2
-        x_g = (x1g + x2g) / 2
-        y_g = (y1g + y2g) / 2
-
-        # area_c = (xc2 - xc1) * (yc2 - yc1)
-        # miouk = iouk - ((area_c - unionk) / (area_c + eps))
-
-        # set_trace()
-        c = 0
-        d = 0
-        # if self.cfg.MODEL.ROI_BOX_HEAD.LOSS_BOX_WEIGHT == 10:
-        #     c = ((xc2 - xc1) ** 2) + ((yc2 - yc1) ** 2) + eps
-        #     d = ((x_p - x_g) ** 2) + ((y_p - y_g) ** 2)
-        # else:
-        c = torch.sqrt(((xc2 - xc1) ** 2) + ((yc2 - yc1) ** 2) + eps)
-        d = torch.sqrt(((x_p - x_g) ** 2) + ((y_p - y_g) ** 2))
-        
-        u = d / c
-        diouk = iouk - u
-
-        loss = ((1 - diouk).sum() / self.gt_classes.numel())
-        loss = loss * self.cfg.MODEL.ROI_BOX_HEAD.LOSS_BOX_WEIGHT
-
-        return loss
+    #Bounding boxes instead of delta values for loss calculation
+    # def compute_diou_fvcore(self):
+    #
+    #     box_dim = self.gt_boxes.tensor.size(1)  # 4 or 5
+    #     device = self.pred_proposal_deltas.device
+    #     bg_class_ind = self.pred_class_logits.shape[1] - 1
+    #     gt_class_cols = torch.arange(box_dim, device=device)
+    #
+    #     fg_inds = nonzero_tuple((self.gt_classes >= 0) & (self.gt_classes < bg_class_ind))[0]
+    #
+    #     boxes1 = self._predict_boxes()[fg_inds[:, None], gt_class_cols]
+    #     boxes2 = self.gt_boxes.tensor[fg_inds]
+    #     eps = 1e-7
+    #
+    #     x1, y1, x2, y2 = boxes1.unbind(dim=-1)
+    #     x1g, y1g, x2g, y2g = boxes2.unbind(dim=-1)
+    #
+    #     assert (x2 >= x1).all(), "bad box: x1 larger than x2"
+    #     assert (y2 >= y1).all(), "bad box: y1 larger than y2"
+    #
+    #     # Intersection keypoints
+    #     xkis1 = torch.max(x1, x1g)
+    #     ykis1 = torch.max(y1, y1g)
+    #     xkis2 = torch.min(x2, x2g)
+    #     ykis2 = torch.min(y2, y2g)
+    #
+    #     intsctk = torch.zeros_like(x1)
+    #     mask = (ykis2 > ykis1) & (xkis2 > xkis1)
+    #     intsctk[mask] = (xkis2[mask] - xkis1[mask]) * (ykis2[mask] - ykis1[mask])
+    #     unionk = (x2 - x1) * (y2 - y1) + (x2g - x1g) * (y2g - y1g) - intsctk
+    #     iouk = intsctk / (unionk + eps)
+    #
+    #     # smallest enclosing box
+    #     xc1 = torch.min(x1, x1g)
+    #     yc1 = torch.min(y1, y1g)
+    #     xc2 = torch.max(x2, x2g)
+    #     yc2 = torch.max(y2, y2g)
+    #
+    #     x_p = (x2 + x1) / 2
+    #     y_p = (y2 + y1) / 2
+    #     x_g = (x1g + x2g) / 2
+    #     y_g = (y1g + y2g) / 2
+    #
+    #     # area_c = (xc2 - xc1) * (yc2 - yc1)
+    #     # miouk = iouk - ((area_c - unionk) / (area_c + eps))
+    #
+    #     # set_trace()
+    #     c = 0
+    #     d = 0
+    #     # if self.cfg.MODEL.ROI_BOX_HEAD.LOSS_BOX_WEIGHT == 10:
+    #     #     c = ((xc2 - xc1) ** 2) + ((yc2 - yc1) ** 2) + eps
+    #     #     d = ((x_p - x_g) ** 2) + ((y_p - y_g) ** 2)
+    #     # else:
+    #     c = torch.sqrt(((xc2 - xc1) ** 2) + ((yc2 - yc1) ** 2) + eps)
+    #     d = torch.sqrt(((x_p - x_g) ** 2) + ((y_p - y_g) ** 2))
+    #
+    #     u = d / c
+    #     diouk = iouk - u
+    #
+    #     loss = ((1 - diouk).sum() / self.gt_classes.numel())
+    #     loss = loss * self.cfg.MODEL.ROI_BOX_HEAD.LOSS_BOX_WEIGHT
+    #
+    #     return loss
 
 
     def bbox_transform(self, deltas, weights):
-        # Using width, height instead of x2, y2. Remove if it doesn't work
-        widths = deltas[:, 2::4] - deltas[:, 0::4]
-        heights = deltas[:, 3::4] - deltas[:, 1::4]
-
         wx, wy, ww, wh = weights
         dx = deltas[:, 0::4] / wx
         dy = deltas[:, 1::4] / wy
-        # dw = deltas[:, 2::4] / ww
-        # dh = deltas[:, 3::4] / wh
-        dw = widths / ww
-        dh = heights / wh
+        dw = deltas[:, 2::4] / ww
+        dh = deltas[:, 3::4] / wh
 
         dw = torch.clamp(dw, max=self.box2box_transform.scale_clamp)
         dh = torch.clamp(dh, max=self.box2box_transform.scale_clamp)
@@ -686,8 +681,8 @@ class FastRCNNOutputs:
         reg_loss = self.cfg.MODEL.ROI_BOX_HEAD.LOSS
 
         if reg_loss == "diou":
-            # losses_dict["loss_box_reg"] = self.compute_diou()
-            losses_dict["loss_box_reg"] = self.compute_diou_fvcore()
+            losses_dict["loss_box_reg"] = self.compute_diou()
+            # losses_dict["loss_box_reg"] = self.compute_diou_fvcore()
         elif reg_loss == "ciou":
             losses_dict["loss_box_reg"] = self.compute_ciou()
         elif reg_loss == "giou":
